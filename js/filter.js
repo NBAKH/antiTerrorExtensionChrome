c9e1663804dee2a3d6e01c3b3fb6768baf4490d5
NBAKH <hr.nbakh@gmail.com>
2016-05-08 20:16:04 +0200

    Name update

--- js/filter.js
+++ js/filter.js
@@ -85,7 +85,7 @@ function iterateThrough(element){
 	// 	replace(children[i], children[i].offsetWidth, children[i].offsetHeight);
 	// }
 	// console.log("grandparent size on x: " + grandParent.offsetWidth +" and on y: "+ grandParent.offsetHeight);
-	
+	var textNumber = Math.floor(Math.random()*texts.length);
 
 	for (var i = 0; i<grandParent.children.length; i++) {	
 		aunts = grandParent.children[i];
@@ -93,7 +93,7 @@ function iterateThrough(element){
 			if(aunts.children[j].tagName != "LI" && aunts.children[j].tagName != "TIME"){
 				try{
 					console.log("all kinds of children: "+aunts.children[j].tagName);
-					replace(aunts.children[j], aunts.children[j].offsetWidth, aunts.children[j].offsetHeight);
+					replace(aunts.children[j], aunts.children[j].offsetWidth, aunts.children[j].offsetHeight,textNumber);
 				}
 				catch(err){console.log("caught exception");}
 			}
@@ -109,16 +109,31 @@ function fade(elements) {
 }
 
 
-function replace(element, width, height){
+function replace(element, width, height, textNumber){
 	var html = '<img src="http://apollo-na-uploads.s3.amazonaws.com/1440642852/saltylol.jpg" ';
 	// console.log("width: " + width);
 	// console.log("height: " + height);
-	if(element.nodeName != 'href'){
-		element.innerHTML = '<'+element.nodeName+'>THIS WOULD HAVE CORRUPTED YOUR BRAIN!!</'+element.nodeName+'> ';
+	
+	switch(element.nodeName){
+		case "H1":
+			element.innerHTML = '<'+element.nodeName+texts[textNumber][0]+element.nodeName+'> ';
+			break;	
+		case "H2":
+			element.innerHTML = '<'+element.nodeName+texts[textNumber][1]+element.nodeName+'> ';
+			break;	
+		case "P":
+			element.innerHTML = '<'+element.nodeName+texts[textNumber][2]+element.nodeName+'> ';
+			break;
+		default:
+			element.innerHTML = texts[textNumber][3] + width + '" width="' + height + '">';
 	}
-	else{
-		element.innerHTML = html + width + '" width="' + height + '">';
-	}
+	
+	// if(element.nodeName != 'href'){
+	// 	element.innerHTML = '<'+element.nodeName+'>THIS WOULD HAVE CORRUPTED YOUR BRAIN!!</'+element.nodeName+'> ';
+	// }
+	// else{
+	// 	element.innerHTML = html + width + '" width="' + height + '">';
+	// }
 }
 
 // Implementation
@@ -140,9 +155,10 @@ if (search) {
   chrome.runtime.sendMessage({}, function(response) {});
 }
 
-var webText=[
-	new Text('></','></','<img src="http://apollo-na-uploads.s3.amazonaws.com/1440642852/saltylol.jpg"'),
-	new Text('></','></','<img src=""'),
-	new Text('></','></','<img src=""'),
-];
+var standardFormat = ['></','></','></','<img src=""']
+var yemenTree = ['>Strange Roots</','>National tree of Yemen<' ,'>Socotra Island, located off the southeast coast of Yemen, is known for its lush biodiversity—and strange plant life. Dracaena cinnabari—more commonly known as the dragon’s blood tree—is the island’s iconic species, with its upturned branches perfectly adapted to capturing moisture.</','<img src="http://images.nationalgeographic.com/wpf/media-live/photos/000/932/cache/socotra-island-ngpc2015_93269_990x742.jpg"'];
+var moroccoFestival = ['>Festival Gnaoua</','>Gnawa music festival held annually in Essaouira, Morocco</','>The festival provides a platform for exchanges and a meeting point of music and dialogue between foreign artists and the mystical Gnaoua (also Gnawa) musicians. In this melting-pot of musical fusion, the Gnaoua masters invite players of jazz, pop, rock and contemporary World music to explore new avenues. The festivals see up to 500,000 visitors every year over four days, many of the performances can be viewed for free, which complicates comparison with other festivals.</','<img src="https://www.fest300.com/system/images/W1siZiIsIjIwMTQvMDQvMTkvMTkvNTEvMzIvNjYvR25hb3VhX011c2ljX0Zlc3RpdmFsX1ZpbmNlX01pbGxldHRfQ0NfaHR0cGZsaWMua3JwNTRCQVl2XzA5LmpwZyJdXQ/Gnaoua_Music_Festival_Vince_Millett_CC_httpflic.krp54BAYv%20-%2009.jpg"']
+
+
+var texts = [yemenTree, moroccoFestival];
 
